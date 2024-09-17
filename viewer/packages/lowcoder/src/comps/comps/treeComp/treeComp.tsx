e3ce54a7b0ddae80ddfd5a0965bfe6dac498653c
Imiss-U1025 <imissu1025@gmail.com>
2024-09-17 23:30:10 +0800

    copy from client and rename it as viewer

---
+++ viewer/packages/lowcoder/src/comps/comps/treeComp/treeComp.tsx
@@ -0,0 +1,209 @@
+import { RecordConstructorToView } from "lowcoder-core";
+import { UICompBuilder } from "comps/generators/uiCompBuilder";
+import { withExposingConfigs } from "comps/generators/withExposing";
+import { Section, sectionNames } from "lowcoder-design";
+import { default as Tree } from "antd/es/tree";
+import { useEffect, useState } from "react";
+import styled from "styled-components";
+import ReactResizeDetector from "react-resize-detector";
+import { StyleConfigType, styleControl } from "comps/controls/styleControl";
+import {  InputFieldStyle, LabelStyle, TreeStyle } from "comps/controls/styleControlConstants";
+import { LabelControl } from "comps/controls/labelControl";
+import { withDefault } from "comps/generators";
+import { dropdownControl } from "comps/controls/dropdownControl";
+import { BoolControl } from "comps/controls/boolControl";
+import {
+  advancedSection,
+  expandSection,
+  formSection,
+  // intersectSection,
+  treeCommonChildren,
+  treeDataPropertyView,
+  TreeNameConfigs,
+  useTree,
+  valuePropertyView,
+} from "./treeUtils";
+import {
+  SelectInputInvalidConfig,
+  SelectInputValidationChildren,
+  SelectInputValidationSection,
+} from "../selectInputComp/selectInputConstants";
+import { selectInputValidate } from "../selectInputComp/selectInputConstants";
+import { SelectEventHandlerControl } from "comps/controls/eventHandlerControl";
+import { trans } from "i18n";
+import { useContext } from "react";
+import { EditorContext } from "comps/editorState";
+
+type TreeStyleType = StyleConfigType<typeof TreeStyle>;
+
+const Container = styled.div<TreeStyleType>`
+  height: 100%;
+  padding: 4px;
+  background: ${(props) => props.background};
+  border: 1px solid ${(props) => props.border};
+  border-radius: ${(props) => props.radius};
+  .ant-tree-show-line .ant-tree-switcher {
+    background: ${(props) => props.background};
+  }
+  .ant-tree:hover .ant-tree-list-scrollbar-show {
+    display: block !important;
+  }
+  .ant-tree-list-scrollbar {
+    width: 6px !important;
+  }
+  .ant-tree-list-scrollbar-thumb {
+    border-radius: 9999px !important;
+    background: rgba(139, 143, 163, 0.2) !important;
+  }
+  .ant-tree-list-scrollbar-thumb:hover {
+    background: rgba(139, 143, 163, 0.5) !important;
+  }
+`;
+
+const selectTypeOptions = [
+  { label: trans("tree.noSelect"), value: "none" },
+  { label: trans("tree.singleSelect"), value: "single" },
+  { label: trans("tree.multiSelect"), value: "multi" },
+  { label: trans("tree.checkbox"), value: "check" },
+] as const;
+
+// TODO: support drag, edit mode
+const childrenMap = {
+  ...treeCommonChildren,
+  selectType: dropdownControl(selectTypeOptions, "single"),
+  checkStrictly: BoolControl,
+  autoExpandParent: BoolControl,
+  label: withDefault(LabelControl, { position: "column" }),
+  // TODO: more event
+  onEvent: SelectEventHandlerControl,
+  style: styleControl(InputFieldStyle , 'style'),
+  labelStyle: styleControl(LabelStyle.filter((style) => ['accent', 'validate'].includes(style.name) === false), 'labelStyle'),
+  inputFieldStyle:styleControl(TreeStyle, 'inputFieldStyle')
+};
+
+const TreeCompView = (props: RecordConstructorToView<typeof childrenMap>) => {
+  const { treeData, selectType, value, expanded, checkStrictly, style, labelStyle } = props;
+  const [height, setHeight] = useState<number>();
+  const selectable = selectType === "single" || selectType === "multi";
+  const checkable = selectType === "check";
+  useEffect(() => {
+    if (selectType === "none" && value.value.length > 0) {
+      value.onChange([]);
+    } else if (selectType === "single" && value.value.length > 1) {
+      value.onChange(value.value.slice(0, 1));
+    }
+  }, [selectType]);
+  useTree(props);
+  return props.label({
+    required: props.required,
+    ...selectInputValidate(props),
+    style,
+    labelStyle,
+    inputFieldStyle:props.inputFieldStyle,
+    children: (
+      <ReactResizeDetector
+        onResize={(w, h) => setHeight(h)}
+        render={() => (
+          <Container {...props.inputFieldStyle}>
+            <Tree
+              key={selectType}
+              disabled={props.disabled}
+              height={height}
+              rootStyle={{ background: "transparent", color: props.inputFieldStyle.text }}
+              fieldNames={{ title: "label", key: "value" }}
+              treeData={treeData}
+              selectable={selectable}
+              multiple={selectType === "multi"}
+              selectedKeys={selectable ? value.value : []}
+              checkable={checkable}
+              checkedKeys={
+                checkable
+                  ? checkStrictly
+                    ? { checked: value.value, halfChecked: [] }
+                    : value.value
+                  : undefined
+              }
+              checkStrictly={checkStrictly}
+              showLine={props.showLine ? { showLeafIcon: props.showLeafIcon } : false}
+              expandedKeys={expanded.value}
+              autoExpandParent={props.autoExpandParent}
+              onSelect={(keys) => {
+                value.onChange(keys as (string | number)[]);
+                props.onEvent("change");
+              }}
+              onCheck={(keys) => {
+                value.onChange(Array.isArray(keys) ? keys as (string | number)[] : keys.checked as (string | number)[]);
+                props.onEvent("change");
+              }}
+              onExpand={(keys) => {
+                expanded.onChange(keys as (string | number)[]);
+              }}
+              onFocus={() => props.onEvent("focus")}
+              onBlur={() => props.onEvent("blur")}
+            />
+        </Container>
+        )}
+      >
+      </ReactResizeDetector>
+    ),
+  });
+};
+
+let TreeBasicComp = (function () {
+  return new UICompBuilder(childrenMap, (props) => {
+    return(<TreeCompView {...props} />)}
+)
+    .setPropertyViewFn((children) => (
+      <>
+        <Section name={sectionNames.basic}>
+          {treeDataPropertyView(children)}
+        </Section>
+
+        {["logic", "both"].includes(useContext(EditorContext).editorModeStatus) && (
+          <><SelectInputValidationSection {...children} />
+            {formSection(children)}
+            <Section name={sectionNames.interaction}>
+              {children.onEvent.getPropertyView()}
+              {children.hidden.propertyView({ label: trans("prop.hide") })}
+              {children.disabled.propertyView({ label: trans("prop.disabled") })}
+              {children.selectType.propertyView({ label: trans("tree.selectType") })}
+              {children.selectType.getView() !== "none" && valuePropertyView(children)}
+              {children.selectType.getView() === "check" &&
+                children.checkStrictly.propertyView({
+                  label: trans("tree.checkStrictly"),
+                  tooltip: trans("tree.checkStrictlyTooltip"),
+                })}
+            </Section>
+          </>
+        )}
+
+        {["layout", "both"].includes(useContext(EditorContext).editorModeStatus) && (
+          <Section name={sectionNames.layout}>
+            {children.expanded.propertyView({ label: trans("tree.expanded") })}
+            {children.defaultExpandAll.propertyView({ label: trans("tree.defaultExpandAll") })}
+            {children.showLine.propertyView({ label: trans("tree.showLine") })}
+            {children.showLine.getView() && children.showLeafIcon.propertyView({ label: trans("tree.showLeafIcon") })}
+          </Section>
+        )}
+
+        {["layout", "both"].includes(useContext(EditorContext).editorModeStatus) && (children.label.getPropertyView())}
+
+        {["layout", "both"].includes(useContext(EditorContext).editorModeStatus) && (
+          <>
+            <Section name={sectionNames.style}>{children.style.getPropertyView()}</Section>
+            <Section name={sectionNames.labelStyle}>{children.labelStyle.getPropertyView()}</Section>
+            <Section name={sectionNames.inputFieldStyle}>{children.inputFieldStyle.getPropertyView()}</Section>
+          </>
+        )}
+      </>
+    ))
+    .build();
+})();
+
+TreeBasicComp = class extends TreeBasicComp {
+  override autoHeight(): boolean {
+    return false;
+  }
+};
+
+export const TreeComp = withExposingConfigs(TreeBasicComp, TreeNameConfigs);
---
+++ viewer/packages/lowcoder/src/pages/editor/AppEditor.tsx
@@ -0,0 +1,196 @@
+import { AppPathParams, AppTypeEnum } from "constants/applicationConstants";
+import { Suspense, lazy, useCallback, useEffect, useMemo, useRef, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { useParams } from "react-router";
+import { AppSummaryInfo, fetchApplicationInfo } from "redux/reduxActions/applicationActions";
+import { fetchDataSourceByApp, fetchDataSourceTypes } from "redux/reduxActions/datasourceActions";
+import { getUser } from "redux/selectors/usersSelectors";
+import { useUserViewMode } from "util/hooks";
+import "comps/uiCompRegistry";
+import { showAppSnapshotSelector } from "redux/selectors/appSnapshotSelector";
+import { setShowAppSnapshot } from "redux/reduxActions/appSnapshotActions";
+import { fetchGroupsAction } from "redux/reduxActions/orgActions";
+import { getFetchOrgGroupsFinished } from "redux/selectors/orgSelectors";
+import { getIsCommonSettingFetching } from "redux/selectors/commonSettingSelectors";
+import {
+  MarkAppDSLLoaded,
+  MarkAppEditorFirstRender,
+  MarkAppEditorMounted,
+  perfClear,
+  perfMark,
+} from "util/perfUtils";
+import { useMount, useUnmount } from "react-use";
+import { fetchQueryLibraryDropdown } from "../../redux/reduxActions/queryLibraryActions";
+import { clearGlobalSettings, setGlobalSettings } from "comps/utils/globalSettings";
+import { fetchFolderElements } from "redux/reduxActions/folderActions";
+import { registryDataSourcePlugin } from "constants/queryConstants";
+import { DatasourceApi } from "api/datasourceApi";
+import { useRootCompInstance } from "./useRootCompInstance";
+import EditorSkeletonView from "./editorSkeletonView";
+import {ErrorBoundary, FallbackProps} from 'react-error-boundary';
+import { ALL_APPLICATIONS_URL } from "@lowcoder-ee/constants/routesURL";
+import history from "util/history";
+import Flex from "antd/es/flex";
+import React from "react";
+
+const AppSnapshot = lazy(() => {
+  return import("pages/editor/appSnapshot")
+    .then(moduleExports => ({default: moduleExports.AppSnapshot}));
+});
+
+const AppEditorInternalView = lazy(
+  () => import("pages/editor/appEditorInternal")
+    .then(moduleExports => ({default: moduleExports.AppEditorInternalView}))
+);
+
+const AppEditor = React.memo(() => {
+  const showAppSnapshot = useSelector(showAppSnapshotSelector);
+  const params = useParams<AppPathParams>();
+  const isUserViewModeCheck = useUserViewMode();
+  const isUserViewMode = params.viewMode ? isUserViewModeCheck : true;
+  const applicationId = params.applicationId || window.location.pathname.split("/")[2];
+  const paramViewMode = params.viewMode || window.location.pathname.split("/")[3];
+  const viewMode = (paramViewMode === "view" || paramViewMode === "admin") ? "published" : paramViewMode === "view_marketplace" ? "view_marketplace" : "editing";
+  const currentUser = useSelector(getUser);
+  const dispatch = useDispatch();
+  const fetchOrgGroupsFinished = useSelector(getFetchOrgGroupsFinished);
+  const isCommonSettingsFetching = useSelector(getIsCommonSettingFetching);
+  const orgId = currentUser.currentOrgId;
+  const firstRendered = useRef(false);
+  const [isDataSourcePluginRegistered, setIsDataSourcePluginRegistered] = useState(false);
+  const [appError, setAppError] = useState('');
+
+  setGlobalSettings({ applicationId, isViewMode: paramViewMode === "view" });
+
+  if (!firstRendered.current) {
+    perfClear();
+    perfMark(MarkAppEditorFirstRender);
+    firstRendered.current = true;
+  }
+
+  useMount(() => {
+    perfMark(MarkAppEditorMounted);
+  });
+
+  useUnmount(() => {
+    clearGlobalSettings();
+  });
+
+  // fetch dsl
+  const [appInfo, setAppInfo] = useState<AppSummaryInfo>({
+    id: "",
+    appType: AppTypeEnum.Application,
+  });
+
+  const readOnly = isUserViewMode;
+  const compInstance = useRootCompInstance(appInfo, readOnly, isDataSourcePluginRegistered);
+
+  // fetch dataSource and plugin
+  useEffect(() => {
+    if (!orgId || paramViewMode !== "edit") {
+      return;
+    }
+    dispatch(fetchDataSourceTypes({ organizationId: orgId }));
+    dispatch(fetchFolderElements({}));
+  }, [dispatch, orgId, paramViewMode]);
+
+  useEffect(() => {
+    if (applicationId && paramViewMode === "edit") {
+      dispatch(fetchDataSourceByApp({ applicationId: applicationId }));
+      dispatch(fetchQueryLibraryDropdown());
+    }
+  }, [dispatch, applicationId, paramViewMode]);
+  
+  const fetchJSDataSourceByApp = () => {
+    DatasourceApi.fetchJsDatasourceByApp(applicationId).then((res) => {
+      res.data.data.forEach((i) => {
+        registryDataSourcePlugin(i.type, i.id, i.pluginDefinition);
+      });
+      setIsDataSourcePluginRegistered(true);
+    });
+    dispatch(setShowAppSnapshot(false));
+  };
+
+  useEffect(() => {
+    if (!fetchOrgGroupsFinished) {
+      dispatch(fetchGroupsAction(orgId));
+    }
+  }, [dispatch, fetchOrgGroupsFinished, orgId]);
+
+  useEffect(() => {
+    dispatch(
+      fetchApplicationInfo({
+        type: viewMode,
+        applicationId: applicationId,
+        onSuccess: (info) => {
+          perfMark(MarkAppDSLLoaded);
+          const runJsInHost =
+            info.orgCommonSettings?.runJavaScriptInHost ?? !!REACT_APP_DISABLE_JS_SANDBOX;
+          setGlobalSettings({
+            orgCommonSettings: {
+              ...info.orgCommonSettings,
+              runJavaScriptInHost: runJsInHost,
+            },
+          });
+          setAppInfo(info);
+          fetchJSDataSourceByApp();
+        },
+        onError: (errorMessage) => {
+          setAppError(errorMessage);
+        }
+      })
+    );
+  }, [viewMode, applicationId, dispatch]);
+
+  const fallbackUI = useMemo(() => (
+    <Flex align="center" justify="center" vertical style={{
+      height: '300px',
+      width: '400px',
+      margin: '0 auto',
+    }}>
+      <h4 style={{margin: 0}}>Something went wrong while displaying this webpage</h4>
+      <button onClick={() => history.push(ALL_APPLICATIONS_URL)} style={{background: '#4965f2',border: '1px solid #4965f2', color: '#ffffff',borderRadius:'6px'}}>Go to Apps</button>
+    </Flex>
+  ), []);
+
+  if (Boolean(appError)) {
+    return (
+      <Flex align="center" justify="center" vertical style={{
+        height: '300px',
+        width: '400px',
+        margin: '0 auto',
+      }}>
+        <h4>{appError}</h4>
+        <button onClick={() => history.push(ALL_APPLICATIONS_URL)} style={{background: '#4965f2',border: '1px solid #4965f2', color: '#ffffff',borderRadius:'6px'}}>Back to Home</button>
+      </Flex>
+    )
+  }
+
+  return (
+    <ErrorBoundary fallback={fallbackUI}>
+      {showAppSnapshot ? (
+        <Suspense fallback={<EditorSkeletonView />}>
+          <AppSnapshot
+            currentAppInfo={{
+              ...appInfo,
+              dsl: compInstance.comp?.toJsonValue() || {},
+            }}
+          />
+        </Suspense>
+      ) : (
+        <Suspense fallback={<EditorSkeletonView />}>
+          <AppEditorInternalView
+            appInfo={appInfo}
+            readOnly={readOnly}
+            loading={
+              !fetchOrgGroupsFinished || !isDataSourcePluginRegistered || isCommonSettingsFetching
+            }
+            compInstance={compInstance}
+          />
+        </Suspense>
+      )}
+    </ErrorBoundary>
+  );
+});
+
+export default AppEditor;
