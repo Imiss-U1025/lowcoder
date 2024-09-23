2ce1cb7e3d32c84850ec30450c71782c4227462e
Imiss-U1025 <imissu1025@gmail.com>
2024-09-23 03:52:01 -0400

    remove language except english

--- viewer/packages/lowcoder/src/i18n/locales/index.ts
+++ viewer/packages/lowcoder/src/i18n/locales/index.ts
@@ -1,11 +1,4 @@
 // file examples: en, enGB, zh, zhHK
 // fallback example: current locale is zh-HK, fallback order is zhHK => zh => en
-export * from "./de"; 
 export * from "./en";
-export * from "./it";
-export * from "./zh";
-
 export * from "./enObj";
-export * from "./deObj";
-export * from "./itObj";
-export * from "./zhObj";
